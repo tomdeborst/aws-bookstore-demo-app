d964fd3d70f383b668679b787d9f8977271a62f8
tpd3 <tom.deborst@gmail.com>
2021-06-04 13:23:31 +1200

    COMPX341-21A4V5

--- assets/src/App.test.tsx
+++ assets/src/App.test.tsx
@@ -4,11 +4,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter, Router } from "react-router-dom"
 
 it('renders without crashing', () => {
   window.alert = jest.fn();
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <BrowserRouter>
+    <App />
+    </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
